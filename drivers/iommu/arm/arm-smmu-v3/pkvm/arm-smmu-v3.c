BayLibre/android_kernel_common

ANDROID: drivers/arm-smmu-v3-kvm: Fix pgsize alignment for IDMAP
When mapping pages in the identity mapped domain,the driver chooses
the most optimal page size based on the requested size.
However, this must be also be aligned to the address.

Bug: 277989609
Bug: 278749606

Change-Id: Id442198eaf33be6b18f3e5198e2f365e279ff6b5
Signed-off-by: Mostafa Saleh <smostafa@google.com>

drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3.c
@@ -1378,13 +1378,19 @@ static phys_addr_t smmu_iova_to_phys(struct kvm_hyp_iommu_domain *domain,
  * As we optimize for memory usage and performance, we try to use block mappings
  * when possible.
  */
-static size_t smmu_pgsize(size_t size)
+static size_t smmu_pgsize_idmap(size_t size, u64 paddr)
 {
 	size_t pgsizes;
 	const size_t pgsize_bitmask = PAGE_SIZE | (PAGE_SIZE * PTRS_PER_PTE) |
 				      (PAGE_SIZE * PTRS_PER_PTE * PTRS_PER_PTE);
 
+	/* All page sizes that fit the size */
 	pgsizes = pgsize_bitmask & GENMASK_ULL(__fls(size), 0);
+
+	/* Address must be aligned to page size */
+	if (likely(paddr))
+		pgsizes &= GENMASK(__ffs(paddr), 0);
+
 	WARN_ON(!pgsizes);
 
 	return BIT(__fls(pgsizes));
@@ -1410,7 +1416,7 @@ static void smmu_host_stage2_idmap(struct kvm_hyp_iommu_domain *domain,
 
 		while (size) {
 			mapped = 0;
-			pgsize = smmu_pgsize(size);
+			pgsize = smmu_pgsize_idmap(size, start);
 			pgcount = size / pgsize;
 			ret = pgtable->ops.map_pages(&pgtable->ops, start, start,
 						     pgsize, pgcount, prot, 0, &mapped);
@@ -1421,7 +1427,7 @@ static void smmu_host_stage2_idmap(struct kvm_hyp_iommu_domain *domain,
 		}
 	} else {
 		while (size) {
-			pgsize = smmu_pgsize(size);
+			pgsize = smmu_pgsize_idmap(size, start);
 			pgcount = size / pgsize;
 			unmapped = pgtable->ops.unmap_pages(&pgtable->ops, start,
 							    pgsize, pgcount, NULL);

omajili-manbu/Xiaomi_Rodin_Kernel_Enhance

drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3.c
@@ -1378,13 +1378,19 @@ static phys_addr_t smmu_iova_to_phys(struct kvm_hyp_iommu_domain *domain,
  * As we optimize for memory usage and performance, we try to use block mappings
  * when possible.
  */
-static size_t smmu_pgsize(size_t size)
+static size_t smmu_pgsize_idmap(size_t size, u64 paddr)
 {
 	size_t pgsizes;
 	const size_t pgsize_bitmask = PAGE_SIZE | (PAGE_SIZE * PTRS_PER_PTE) |
 				      (PAGE_SIZE * PTRS_PER_PTE * PTRS_PER_PTE);
 
+	/* All page sizes that fit the size */
 	pgsizes = pgsize_bitmask & GENMASK_ULL(__fls(size), 0);
+
+	/* Address must be aligned to page size */
+	if (likely(paddr))
+		pgsizes &= GENMASK_ULL(__ffs(paddr), 0);
+
 	WARN_ON(!pgsizes);
 
 	return BIT(__fls(pgsizes));
@@ -1410,7 +1416,7 @@ static void smmu_host_stage2_idmap(struct kvm_hyp_iommu_domain *domain,
 
 		while (size) {
 			mapped = 0;
-			pgsize = smmu_pgsize(size);
+			pgsize = smmu_pgsize_idmap(size, start);
 			pgcount = size / pgsize;
 			ret = pgtable->ops.map_pages(&pgtable->ops, start, start,
 						     pgsize, pgcount, prot, 0, &mapped);
@@ -1421,7 +1427,7 @@ static void smmu_host_stage2_idmap(struct kvm_hyp_iommu_domain *domain,
 		}
 	} else {
 		while (size) {
-			pgsize = smmu_pgsize(size);
+			pgsize = smmu_pgsize_idmap(size, start);
 			pgcount = size / pgsize;
 			unmapped = pgtable->ops.unmap_pages(&pgtable->ops, start,
 							    pgsize, pgcount, NULL);