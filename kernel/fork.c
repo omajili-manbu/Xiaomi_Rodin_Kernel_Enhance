275440d13fd0ea81342948baa6a55708ca509567
fanshaohua <fanshaohua@xiaomi.corp-partner.google.com>
2026-01-09 18:31:07 +0800

    ANDROID: vendor_hooks: Add vendor hooks for mmap_lock
    Add two hooks in mmap_lock init and free stage
    Move func mmap_init_lock from include/linux/mmap_lock.h
    to kernel/fork.c
    
    Bug: 467835301
    Bug: 493943094
    Change-Id: I78973ef14886f7ba9d053a7a2811966e6e04a31b
    Signed-off-by: fanshaohua <fanshaohua@xiaomi.corp-partner.google.com>
    (cherry picked from commit 9bd4e3d79beb702a17521a728b82b02cad4773b0)

--- kernel/fork.c
+++ kernel/fork.c
@@ -957,6 +957,7 @@ void __mmdrop(struct mm_struct *mm)
 	mm_pasid_drop(mm);
 	mm_destroy_cid(mm);
 	percpu_counter_destroy_many(mm->rss_stat, NR_MM_COUNTERS);
+	trace_android_vh_mmap_lock_free(&mm->mmap_lock);
 
 	trace_android_vh_mm_free(mm);
 	free_mm(mm);
@@ -1293,6 +1294,12 @@ static void mm_init_uprobes_state(struct mm_struct *mm)
 #ifdef CONFIG_UPROBES
 	mm->uprobes_state.xol_area = NULL;
 #endif
+}
+
+static void mmap_init_lock(struct mm_struct *mm)
+{
+	init_rwsem(&mm->mmap_lock);
+	trace_android_vh_mmap_lock_init(&mm->mmap_lock);
 }
 
 static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
