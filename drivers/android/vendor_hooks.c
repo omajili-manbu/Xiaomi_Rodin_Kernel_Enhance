bfca1c5ea2994abdab40f6ad7f5d23b21b114056
Dongdong zhang <zhangdongdong5@xiaomi.com>
2026-03-03 11:04:57 +0800

    ANDROID: mm: add vendor hook to trace swap bio charge
    Add a vendor hook `android_rvh_swap_bio_charge` in swap read path
    to allow vendors to monitor  behavior when a swap bio
    is submitted.
    
    Bug: 489211177
    
    Change-Id: I6363c7b9b4be1bce209ee9013adf49a1e02dd193
    Signed-off-by: Dongdong zhang <zhangdongdong5@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -722,3 +722,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_restore_curr_resched);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_curr_lazy);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folios_put_refs_direct_free_extent);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_pages_and_swap_cache);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_swap_bio_charge);
