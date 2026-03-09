BayLibre/android_kernel_common

ANDROID: GKI: Add to task_struct size via cmdline
To reduce the size of vendor data allocated in the task_struct, from 512
bytes to a significantly lower 48 bytes, the move to a dynamically sized
task_struct is being made.
As part of this effort, provide means for vendors to pass a size value
via kernel cmdline. Use the passed value to dynamically add to the
task_struct size to accommodate vendor data.
The cmdline parameter to be used is 'task_struct_vendor_size'. For eg.,
vendors can add the following to the bootargs section of their
devicetree to add an extra 512 bytes to the task_struct:
"task_struct_vendor_size=512"
To access this additional memory, simply do p + 1, to get the starting
address, where 'p' is the pointer to a task's task_struct structure.

Bug: 233921394
Change-Id: I6d5ab92080b82f29bbe9735d40f7d0b1e5bb5913
Signed-off-by: Sai Harshini Nimmala <quic_snimmala@quicinc.com>

kernel/fork.c
@@ -1034,6 +1034,24 @@ static void __init set_max_threads(unsigned int max_threads_suggested)
 #ifdef CONFIG_ARCH_WANTS_DYNAMIC_TASK_STRUCT
 /* Initialized by the architecture: */
 int arch_task_struct_size __read_mostly;
+
+static int __init arch_task_struct_size_setup(char *str)
+{
+	u64 size;
+
+	if (!str)
+		return -EINVAL;
+
+	size = memparse(str, &str);
+
+	if (size < 0 || size > TASK_STRUCT_VENDOR_SIZE_MAX)
+		return -EINVAL;
+
+	arch_task_struct_size = sizeof(struct task_struct) + size;
+
+	return 0;
+}
+early_param("task_struct_vendor_size", arch_task_struct_size_setup);
 #endif
 
 static void __init task_struct_whitelist(unsigned long *offset, unsigned long *size)

omajili-manbu/Xiaomi_Rodin_Kernel_Enhance

The cmdline parameter to be used is 'android_task_struct_vendor_size'.
For eg., vendors can add the following to the bootargs section of their
"android_task_struct_vendor_size=512"
To access this additional memory, use the android_task_vendor_data
function provided.
(Cherry picked from 5e9a8cb7144f7dfe4e2eac71f767c6327e4af8b2)
Bug: 489588860
Signed-off-by: Jihan LIN <linjihan@xiaomi.com>

kernel/fork.c
@@ -1052,6 +1052,24 @@ static void set_max_threads(unsigned int max_threads_suggested)
 #ifdef CONFIG_ARCH_WANTS_DYNAMIC_TASK_STRUCT
 /* Initialized by the architecture: */
 int arch_task_struct_size __read_mostly;
+
+static int __init arch_task_struct_size_setup(char *str)
+{
+	u64 size;
+
+	if (!str)
+		return -EINVAL;
+
+	size = memparse(str, &str);
+
+	if (size < 0 || size > CONFIG_GKI_TASK_STRUCT_VENDOR_SIZE_MAX)
+		return -EINVAL;
+
+	arch_task_struct_size = sizeof(struct task_struct) + size;
+
+	return 0;
+}
+early_param("android_task_struct_vendor_size", arch_task_struct_size_setup);
 #endif
 
 #ifndef CONFIG_ARCH_TASK_STRUCT_ALLOCATOR