c28a665be2f152277b5f9290be5dcfb95ffea652
Greg Kroah-Hartman <gregkh@google.com>
2026-03-17 07:03:43 +0000

    Revert "usb: dwc3: gadget: Move vbus draw to workqueue context"
    This reverts commit 76c1123ffccfaba95cf4ecc2a50f95504a522424 which is
    commit 54aaa3b387c2f580a99dc86a9cc2eb6dfaf599a7 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I71671e989d734f1cc130d06bf52d497f125f93eb
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -3141,6 +3141,8 @@ static void dwc3_gadget_set_ssp_rate(struct usb_gadget *g,
 static int dwc3_gadget_vbus_draw(struct usb_gadget *g, unsigned int mA)
 {
 	struct dwc3		*dwc = gadget_to_dwc(g);
+	union power_supply_propval	val = {0};
+	int				ret;
 
 	if (dwc->usb2_phy)
 		return usb_phy_set_power(dwc->usb2_phy, mA);
@@ -3148,10 +3150,10 @@ static int dwc3_gadget_vbus_draw(struct usb_gadget *g, unsigned int mA)
 	if (!dwc->usb_psy)
 		return -EOPNOTSUPP;
 
-	dwc->current_limit = mA;
-	schedule_work(&dwc->vbus_draw_work);
+	val.intval = 1000 * mA;
+	ret = power_supply_set_property(dwc->usb_psy, POWER_SUPPLY_PROP_INPUT_CURRENT_LIMIT, &val);
 
-	return 0;
+	return ret;
 }
 
 /**
