maltesermailo/android_kernel_pantah

ANDROID: vendor_hooks: add vendor hook in cma_alloc()
By this vh, so that we can reimplement the function cma_alloc()

Bug: 299425220

Change-Id: I7068915c3c38784dba25d130434abe595b06b996
Signed-off-by: wudihui <wudihui@xiaomi.corp-partner.google.com>

mm/cma.c
@@ -471,6 +471,12 @@ struct page *__cma_alloc(struct cma *cma, unsigned long count,
 	int ret = -ENOMEM;
 	int num_attempts = 0;
 	int max_retries = 5;
+	bool bypass = false;
+
+	trace_android_vh_cma_alloc_bypass(cma, count, align, no_warn,
+				&page, &bypass);
+	if (bypass)
+		return page;
 
 	if (WARN_ON_ONCE((gfp_mask & GFP_KERNEL) == 0 ||
 		(gfp_mask & ~(GFP_KERNEL|__GFP_NOWARN|__GFP_NORETRY)) != 0))

omajili-manbu/Xiaomi_Rodin_Kernel_Enhance

mm/cma.c
@@ -471,6 +471,7 @@ struct page *__cma_alloc(struct cma *cma, unsigned long count,
 	int ret = -ENOMEM;
 	int num_attempts = 0;
 	int max_retries = 5;
+	bool bypass = false;
 
 	if (WARN_ON_ONCE((gfp_mask & GFP_KERNEL) == 0 ||
 		(gfp_mask & ~(GFP_KERNEL|__GFP_NOWARN|__GFP_NORETRY)) != 0))
@@ -478,6 +479,11 @@ struct page *__cma_alloc(struct cma *cma, unsigned long count,
 
 	if (!cma || !cma->count || !cma->bitmap)
 		goto out;
+
+	trace_android_vh_cma_alloc_bypass(cma, count, align, gfp_mask,
+				&page, &bypass);
+	if (bypass)
+		return page;
 
 	pr_debug("%s(cma %p, name: %s, count %lu, align %d)\n", __func__,
 		(void *)cma, cma->name, count, align);