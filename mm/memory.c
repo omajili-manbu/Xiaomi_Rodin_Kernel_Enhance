d7e5738022c4348f00da561de47967f7e568c12f
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2026-05-22 18:02:10 +0900

    ANDROID: mm: add vendor hook for do_swap_page
    Add vendor hooks to replace global swapcache waitqueue with hash-based
    waitqueue.
    
    Bug: 514180992
    Change-Id: I9b789221bb237ae274dace3968abcb0367b8e7d5
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- mm/memory.c
+++ mm/memory.c
@@ -4169,6 +4169,7 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 	unsigned long page_idx;
 	unsigned long address;
 	pte_t *ptep;
+	bool bypass = false;
 
 	if (!pte_unmap_same(vmf))
 		goto out;
@@ -4255,6 +4256,9 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 					 * Relax a bit to prevent rapid
 					 * repeated page faults.
 					 */
+					trace_android_rvh_do_swap_page_relax(entry, &bypass);
+					if (bypass)
+						goto out_page;
 					add_wait_queue(&swapcache_wq, &wait);
 					schedule_timeout_uninterruptible(1);
 					remove_wait_queue(&swapcache_wq, &wait);
@@ -4570,6 +4574,7 @@ out:
 		swapcache_clear(si, entry, nr_pages);
 		if (waitqueue_active(&swapcache_wq))
 			wake_up(&swapcache_wq);
+		trace_android_vh_do_swap_page_done(entry);
 	}
 	if (si)
 		put_swap_device(si);
@@ -4589,6 +4594,7 @@ out_release:
 		swapcache_clear(si, entry, nr_pages);
 		if (waitqueue_active(&swapcache_wq))
 			wake_up(&swapcache_wq);
+		trace_android_vh_do_swap_page_done(entry);
 	}
 	if (si)
 		put_swap_device(si);
